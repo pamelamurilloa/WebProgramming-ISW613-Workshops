37c2f6c501a6bcac095eb6d09cf87d66ff5eb6e5
Pamela Murillo <109484143+pamelamurilloa@users.noreply.github.com>
2023-09-18 22:05:23 -0600

    Creation of a simple php file with a single line displaying date and time in web, the meta details in the head section refreshes the page every second and CSS file to add style to the index html main file
    code.php - Creation of a simple php file with a single line displaying date and time in web, the meta details in the head section refreshes the page every second
    
    index.html - Main file, it contains the bare minimum with a title, a subtitle and an object with the connection to he php file to display the code

---
+++ Workshop-1/code.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="1"> <!-- Refreshes the page every second-->
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Code</title>
+</head>
+    <body>
+        <p>
+            <?php
+
+                echo date('Y-m-d H:i:s'); //Prints in web the current date and time
+
+            ?>
+
+        </p>
+    </body>
+</html>
+
+
+
