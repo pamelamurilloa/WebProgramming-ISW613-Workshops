a92009d55f44c2904377c58983ed9cb12d87aff1
Pamela Murillo <109484143+pamelamurilloa@users.noreply.github.com>
2023-10-02 23:41:25 -0600

    Light details with css

--- Workshop-2/users.php
+++ Workshop-2/users.php
@@ -10,6 +10,7 @@ $users = getUsers();
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="style/users.css">
   <title>UserInformation</title>
 </head>
 <body>
