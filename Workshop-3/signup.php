8dc4ebc009f2685eee1538706d6cfb0ce355a87d
Pamela Murillo <109484143+pamelamurilloa@users.noreply.github.com>
2023-10-10 01:09:32 -0600

    Role and Username has been added to the register option. It now directs to the index instead of the users file

--- Workshop-3/signup.php
+++ Workshop-3/signup.php
@@ -6,16 +6,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $user['firstName'] = $_POST["firstName"];
     $user['lastName'] = $_POST['lastName'];
+    $user['password'] = $_POST['password'];
+    $user['userName'] = $_POST['userName'];
     $user['email'] = $_POST['email'];
     $user['provinceID'] = $_POST['province'];
-    $user['password'] = $_POST['password'];
+    $user['role'] = 'normal_user';
 
   if (saveUser($user)) {
-    header("Location: http://localhost/WebProgramming-Workshops/Workshop-2/users.php");
+    header("Location: http://localhost/WebProgramming-Workshops/Workshop-3/index.php");
   } else {
-  header("Location: http://localhost/WebProgramming-Workshops/Workshop-2/?error=true");
+    header("Location: http://localhost/WebProgramming-Workshops/Workshop-3/?error=true");
   }
-
 
 }
 ?>
