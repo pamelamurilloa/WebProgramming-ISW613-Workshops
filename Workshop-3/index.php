5796c35d7f3ef5e41240eba77c1d3fb8a446ed80
Pamela Murillo <109484143+pamelamurilloa@users.noreply.github.com>
2023-10-10 01:10:32 -0600

    A few comments have been deleted and the unnecesary IDs have been deleted as well. The button is now functional

--- Workshop-3/index.php
+++ Workshop-3/index.php
@@ -1,22 +1,3 @@
-<?php
-  $message = "";
-  // session_start();
-  // if ($_SESSION && $_SESSION['user']){
-  //   //user already logged in
-  //   header('Location: dashboard.php');
-  // }
-
-  // if(!empty($_REQUEST['status'])) {
-  //   switch($_REQUEST['status']) {
-  //     case 'login':
-  //       $message = 'User does not exists';
-  //     break;
-  //     case 'error':
-  //       $message = 'There was a problem inserting the user';
-  //     break;
-  //   }
-  // }
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,30 +5,25 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-  <!-- Latest compiled and minified CSS -->
   <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 
   <title>Document</title>
 </head>
 <body>
 <div class="container">
-    <div class="msg">
-      <?php echo $message; ?>
-    </div>
     <h1>User Login</h1>
     <form action="login.php" method="POST" class="form-inline" role="form">
       <div class="form-group">
         <label class="sr-only" for="">Username</label>
-        <input type="text" class="form-control" id="" name="username" placeholder="Your username">
+        <input type="text" class="form-control" name="username" placeholder="Your username">
       </div>
       <div class="form-group">
         <label class="sr-only" for="">Password</label>
-        <input type="password" class="form-control" id="" name="password" placeholder="Your password">
+        <input type="password" class="form-control" name="password" placeholder="Your password">
       </div>
 
-      <!-- <button type="submit" class="btn btn-primary">Login</button> -->
       <input type="submit" class="btn btn-primary" value="Login"></input>
 
-      <a class="btn">Clear</a>
+      <a class="btn" href="/WebProgramming-Workshops/Workshop-3/users/addUser.php">Sign Up</a>
 </body>
 </html>
