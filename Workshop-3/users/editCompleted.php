74eb8a1933f56fda96d188ac3fdb6138449238a9
Pamela Murillo <109484143+pamelamurilloa@users.noreply.github.com>
2023-10-10 01:12:11 -0600

    This file takes the information from the edit options and updates it

---
+++ Workshop-3/users/editCompleted.php
@@ -0,0 +1,25 @@
+<?php
+
+include('../utils/functions.php');
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $user['id'] = $_POST["id"];
+    $user['firstname'] = $_POST["firstName"];
+    $user['lastname'] = $_POST['lastName'];
+    $user['password'] = $_POST['password'];
+    $user['username'] = $_POST['userName'];
+    $user['email'] = $_POST['email'];
+    $user['provinceID'] = $_POST['province'];
+    $user['role'] = $_POST['role'];
+
+  if (updateUser($user)) {
+    header("Location: http://localhost/WebProgramming-Workshops/Workshop-3/showUsers.php");
+  } else {
+    header("Location: http://localhost/WebProgramming-Workshops/Workshop-3/?error=true");
+  }
+
+
+}
+?>
