0caa936c0d8ba5d993e7be9d24fa47f480578c69
Pamela Murillo <109484143+pamelamurilloa@users.noreply.github.com>
2023-10-17 11:05:11 -0600

    The functions prevously added are now called in index

--- Workshop-5/index.php
+++ Workshop-5/index.php
@@ -30,6 +30,8 @@ $ceu = array(
   "Poland"=>"Warsaw"
 ) ;
 
+$temperatures = [78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65, 74, 62, 62, 65, 64, 68, 73, 75, 79, 73];
+
 ?>
 
 <!DOCTYPE html>
@@ -39,15 +41,23 @@ $ceu = array(
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-  <link rel="stylesheet" type="text" href="style.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
 
   <title>Workshop 5</title>
 </head>
 <body>
-  <div class="country_list">
-    <?php
-      printCountryWithCapital($ceu);
-    ?>
-    
+  <div class="container" >
+    <div class="country_list">
+      <?php
+        printCountryWithCapital($ceu);
+      ?>
+    </div>
+    <div class="temperatures">
+      <?php
+        getAverageFromArray($temperatures);
+        getLowestHighestNumberInArray(5, 5, $temperatures);
+      ?>
+    </div>
+  </div>
 </body>
 </html>
