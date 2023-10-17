9d7ddedeffde97a4035229625b434b56d6cf0aa6
Pamela Murillo <109484143+pamelamurilloa@users.noreply.github.com>
2023-10-17 11:20:03 -0600

    Setup of workshop 6

---
+++ Workshop-6/index.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once("functions.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <link rel="stylesheet" type="text/css" href="style.css">
+
+  <title>Workshop 5</title>
+</head>
+<body>
+  <div class="container" >
+
+  </div>
+</body>
+</html>
