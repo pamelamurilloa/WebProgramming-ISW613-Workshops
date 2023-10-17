1fe28fe9975530edf27d01d590d8d7495d07b24e
Pamela Murillo <109484143+pamelamurilloa@users.noreply.github.com>
2023-10-17 11:04:45 -0600

    Two new functions were added to get the average and the lowest and highest numbers in an array

--- Workshop-5/functions.php
+++ Workshop-5/functions.php
@@ -12,3 +12,19 @@ function printCountryWithCapital (&$countryArray) {
       }
 
 }
+
+function getAverageFromArray($numberArray) {
+    $average = array_sum($numberArray)/count($numberArray);
+    echo "The average temperature is " . round($average, 2);
+    echo "<br>";
+}
+
+function getLowestHighestNumberInArray ($top, $bottom, $numberArray) {
+    sort($numberArray);
+    $numberArray = array_unique($numberArray);
+    $bottomNumbers = array_slice($numberArray, count($numberArray)-$bottom, count($numberArray) );
+    $topNumbers = array_slice($numberArray, 0, $top);
+    echo "List of " . $top . " highest temperatures are " . implode(", ", $topNumbers);
+    echo "<br>";
+    echo "List of " . $bottom . " lowest temperatures are " . implode(", ", $bottomNumbers);
+}
