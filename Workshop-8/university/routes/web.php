95a6454aeba5b9513445d64da129bfcfdb3d6580
Pamela Murillo <pamelamurilloa@gmail.com>
2023-12-04 22:28:07 -0600

    Views, models and controllers created

--- Workshop-8/university/routes/web.php
+++ Workshop-8/university/routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Rutas de Estudiantes
+Route::resource('students', 'StudentController');
+
+// Rutas de Carreras
+Route::resource('carreras', 'CarreraController');
