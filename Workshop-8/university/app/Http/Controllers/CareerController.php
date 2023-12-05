cc7ca3a742e805ca14200c754e9a7398ac952e14
Pamela Murillo <pamelamurilloa@gmail.com>
2023-12-04 23:07:57 -0600

    Added File directioning configuration

--- Workshop-8/university/app/Http/Controllers/CareerController.php
+++ Workshop-8/university/app/Http/Controllers/CareerController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Career;
+
 
 class CareerController extends Controller
 {
--- Workshop-8/university/app/Models/Career.php
+++ Workshop-8/university/app/Models/Career.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Career extends Model
 {
     use HasFactory;
+
+    protected $table = 'careers';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+                            'name'
+                          ];
 }
